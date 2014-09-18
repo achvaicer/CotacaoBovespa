069a09c193246fb9b5f5f319c52fccd666213bc0
Alberto Chvaicer <achvaicer@gmail.com>
2014-09-18 10:14:07 -0300

    fixing tests

--- src/CotacaoBovespa.Tests/BuscaCotacao.cs
+++ src/CotacaoBovespa.Tests/BuscaCotacao.cs
@@ -14,59 +14,52 @@ namespace CotacaoBovespa.Tests
         const string bluechip = "PETR4";
         const string smallcap = "TELB4";
 
-        private static string Cotacao(string codigoAcao, string funcao)
-        {
-            var acao = new Acao();
-            var retorno = acao.Cotacao(codigoAcao, funcao);
-            return retorno;
-        }
-
         [Test]
         public void passing_codigoAcao_as_null()
         {
-            var preco = Cotacao(null, null);
+            var preco = Acao.Cotacao(null, null);
             Assert.IsEmpty(preco);
         }
 
         [Test]
         public void passing_codigoAcao_as_empty()
         {
-            var preco = Cotacao("", null);
+            var preco = Acao.Cotacao("", null);
             Assert.IsEmpty(preco);
         }
 
         [Test]
         public void bluechip_passing_funcao_as_null()
         {
-            var preco = decimal.Parse(Cotacao(bluechip, null));
+            var preco = decimal.Parse(Acao.Cotacao(bluechip, null));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
         [Test]
         public void bluechip_passing_funcao_as_empty()
         {
-            var preco = decimal.Parse(Cotacao(bluechip, ""));
+            var preco = decimal.Parse(Acao.Cotacao(bluechip, ""));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
         [Test]
         public void bluechip_preco()
         {
-            var preco = decimal.Parse(Cotacao(bluechip, "preco"));
+            var preco = decimal.Parse(Acao.Cotacao(bluechip, "preco"));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
         [Test]
         public void smallcap_preco()
         {
-            var preco = decimal.Parse(Cotacao(smallcap, "preco"));
+            var preco = decimal.Parse(Acao.Cotacao(smallcap, "preco"));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
         [Test]
         public void bluechip_maxima()
         {
-            var preco = decimal.Parse(Cotacao(bluechip, "maxima"));
+            var preco = decimal.Parse(Acao.Cotacao(bluechip, "maxima"));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
@@ -75,35 +68,35 @@ namespace CotacaoBovespa.Tests
         [Test]
         public void smallcap_maxima()
         {
-            var preco = decimal.Parse(Cotacao(smallcap, "maxima"));
+            var preco = decimal.Parse(Acao.Cotacao(smallcap, "maxima"));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
         [Test]
         public void bluechip_historica()
         {
-            var preco = decimal.Parse(Cotacao(bluechip, "2011-10-20"));
+            var preco = decimal.Parse(Acao.Cotacao(bluechip, "2011-10-20"));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
         [Test]
         public void smallcap_historica()
         {
-            var preco = decimal.Parse(Cotacao(smallcap, "2011-10-20"));
+            var preco = decimal.Parse(Acao.Cotacao(smallcap, "2011-10-20"));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
         [Test]
         public void bluechip_minima()
         {
-            var preco = decimal.Parse(Cotacao(bluechip, "minima"));
+            var preco = decimal.Parse(Acao.Cotacao(bluechip, "minima"));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
         [Test]
         public void smallcap_minima()
         {
-            var preco = decimal.Parse(Cotacao(smallcap, "minima"));
+            var preco = decimal.Parse(Acao.Cotacao(smallcap, "minima"));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
@@ -111,56 +104,56 @@ namespace CotacaoBovespa.Tests
         [Test]
         public void bluechip_abertura()
         {
-            var preco = decimal.Parse(Cotacao(bluechip, "abertura"));
+            var preco = decimal.Parse(Acao.Cotacao(bluechip, "abertura"));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
         [Test]
         public void smallcap_abertura()
         {
-            var preco = decimal.Parse(Cotacao(smallcap, "abertura"));
+            var preco = decimal.Parse(Acao.Cotacao(smallcap, "abertura"));
             Assert.IsInstanceOf<decimal>(preco);
         }
 
         [Test]
         public void bluechip_volume()
         {
-            var volume = Cotacao(bluechip, "volume");
+            var volume = Acao.Cotacao(bluechip, "volume");
             Assert.IsTrue(Regex.IsMatch(volume, @"^(\d{1,3}.(\d{3}.)*\d{3}(\,\d{1,3})?|\d{1,3}(\,\d{3})?)$"));
         }
 
         [Test]
         public void smallcap_volume()
         {
-            var volume = Cotacao(smallcap, "volume");
+            var volume = Acao.Cotacao(smallcap, "volume");
             Assert.IsTrue(Regex.IsMatch(volume, @"^(\d{1,3}.(\d{3}.)*\d{3}(\,\d{1,3})?|\d{1,3}(\,\d{3})?)$"));
         }
 
         [Test]
         public void bluechip_variacao()
         {
-            var variacao = Cotacao(bluechip, "variacao");
+            var variacao = Acao.Cotacao(bluechip, "variacao");
             Assert.IsTrue(Regex.IsMatch(variacao, @"^(-?(\d{1,2})?([,][\d]{1,2})?){1}[%]{1}$"));
         }
 
         [Test]
         public void smallcap_variacao()
         {
-            var variacao = Cotacao(smallcap, "variacao");
+            var variacao = Acao.Cotacao(smallcap, "variacao");
             Assert.IsTrue(Regex.IsMatch(variacao, @"^(-?(\d{1,2})?([,][\d]{1,2})?){1}[%]{1}$"));
         }
 
         [Test]
         public void bluechip_hora()
         {
-            var hora = Cotacao(bluechip, "hora");
+            var hora = Acao.Cotacao(bluechip, "hora");
             Assert.IsTrue(Regex.IsMatch(hora, @"^(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])?$"));
         }
 
         [Test]
         public void smallcap_hora()
         {
-            var hora = Cotacao(smallcap, "hora");
+            var hora = Acao.Cotacao(smallcap, "hora");
             Assert.IsTrue(Regex.IsMatch(hora, @"^(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])?$"));
         }
     }
